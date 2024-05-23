cc638246a97fdcd926ac318745b68a031637388b
Donquis <juandonquis07@gmail.com>
2024-05-23 12:23:41 -0400

    add fields to machines and search

--- app/Services/MachineService.php
+++ app/Services/MachineService.php
@@ -23,7 +23,41 @@ class MachineService
         {
             $query->where('search','like','%' . $search . '%');
         })
+        ->with('location','office','division','department')
         ->paginate(25)
+        ->withQueryString()
+        ->through(fn($machine) => [
+
+            "id" => $machine->id,
+            "code" => $machine->code,
+            "name" => $machine->name,
+            "brand" => $machine->brand,
+            "model" => $machine->model,
+            "manufacturer" => $machine->manufacturer,
+            "serial_number" => $machine->serial_number,
+            "location_id" => $machine->location->id,
+            "location_name" => $machine->location->name,
+            "office_id" => $machine->office->id,
+            "office_name" => $machine->office->name,
+            "division_id" => $machine->division->id,
+            "division_name" => $machine->division->name,
+            "department_id" => $machine->department->id,
+            "department_name" => $machine->department->name,
+            "photo" => $machine->photo,
+            "observation" => $machine->observation
+        ]);
+
+        return $machines;
+    }
+
+    public function searchMachines($request)
+    {
+        $machines = Machine::query()
+        ->when($request->input('search'), function ($query, $search) 
+        {
+            $query->where('search','like','%' . $search . '%');
+        })
+        ->get()
         ->withQueryString()
         ->through(fn($machine) => [
 
@@ -52,12 +86,18 @@ class MachineService
             "model",
             "manufacturer",
             "serial_number",
+            "location_id",
+            "office_id",
+            "division_id",
+            "department_id",
             "observation"
         ]));
 
         if($request->hasFile('photo'))
             $this->handlePhoto($request,$newMachine);
 
+
+        $newMachine->load('location','division','office','department');
 
         $search = $this->generateSearch($newMachine);
 
@@ -79,11 +119,17 @@ class MachineService
                 "model" => $request->input('model'),
                 "manufacturer" => $request->input('manufacturer'),
                 "serial_number" => $request->input('serial_number'),
+                "location_id" => $request->input('location_id'),
+                "office_id" => $request->input('office_id'),
+                "division_id" => $request->input('division_id'),
+                "department_id" => $request->input('department_id'),
                 "observation" => $request->input('observation')
 
             ]
         );
-    
+        
+        $machine->load('location','division','office','department');
+
 
         $search = $this->generateSearch($machine);
         
@@ -100,6 +146,10 @@ class MachineService
          . $machine->model . ' '
          . $machine->manufacturer . ' '
          . $machine->serial_number . ' '
+         . $machine->location->name . ' '
+         . $machine->office->name . ' '
+         . $machine->division->name . ' '
+         . $machine->department->name . ' '
          . $machine->observation ?? null . ' ';
          
          return $search;
